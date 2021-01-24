0fb822097cbe93ba4cb1ce278a140d2d1b90bd90
WinLinux1028 <exsample@protonmail.com>
2021-01-25 00:59:34 +0900

    big update

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,110 @@
 use std::sync::Mutex;
 
-//First, you have to write in your program like:
-//static FOO: Global<String> = Global::No;
-pub enum Global<T> {
-    No,
-    Yes(Mutex<T>),
+/// # The safety global variable wrapper
+/// You should use this if you have a special reason
+/// ## Examples
+/// ```
+/// use useful_static::Global;
+///
+/// static TEST: Global<String> = Global::new();
+///
+/// fn main() {
+///     TEST.set(String::from("Hello World!"));
+///     println!("{}", TEST.lock().unwrap()) //Hello World!
+/// }
+/// ```
+/// ## Panics
+/// If you use `deref` method to this without `.set` method, It will panics.
+/// ```
+/// use useful_static::Global;
+///
+/// static TEST: Global<String> = Global::new();
+///
+/// fn main() {
+///     println!("{}", TEST.lock().unwrap()) //Panic!
+/// }
+/// ```
+pub struct Global<T> {
+    content: RawGlobal<Mutex<T>>,
 }
 
 impl<T> Global<T> {
-    //Second, set value by this method.
+    ///Create new `Global` instance.
+    pub const fn new() -> Global<T> {
+        Global {
+            content: RawGlobal::new(),
+        }
+    }
+
+    ///Set value to `Global`.
+    pub fn set(&self, content: T) {
+        self.content.set(Mutex::new(content));
+    }
+}
+
+impl<T> std::ops::Deref for Global<T> {
+    type Target = Mutex<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &*self.content
+    }
+}
+
+/// # The global variable for other Mutex
+/// If you want to use other Mutex, you should use this.
+/// This will useful to read-only global variable too.
+/// ## Examples
+/// ```
+/// use useful_static::RawGlobal;
+/// use tokio::sync::Mutex;
+///
+/// static TEST: RawGlobal<Mutex<String>> = RawGlobal::new();
+///
+/// #[tokio::main]
+/// async fn main() {
+///     TEST.set(Mutex::new(String::from("Hello World!")));
+///     println!("{}", TEST.lock().await) //Hello World!
+/// }
+/// ```
+/// ## Panics
+/// If you use `deref` method to this without `.set` method, It will panics.
+/// ```
+/// use useful_static::RawGlobal;
+/// use std::sync::Mutex;
+///
+/// static TEST: RawGlobal<Mutex<String>> = RawGlobal::new();
+///
+/// fn main() {
+///     println!("{}", TEST.lock().unwrap()) //Panic!
+/// }
+/// ```
+pub enum RawGlobal<T> {
+    No(Option<Mutex<()>>),
+    Yes(T),
+}
+
+impl<T> RawGlobal<T> {
+    ///Create new `RawGlobal` instance.
+    pub const fn new() -> RawGlobal<T> {
+        RawGlobal::No(None)
+    }
+
+    /// Set value to `RawGlobal`.
     pub fn set(&self, content: T) {
         unsafe {
             let global = self as *const _ as *mut _;
-            *global = Global::Yes(Mutex::new(content));
+            *global = RawGlobal::Yes(content);
         }
     }
 }
 
-impl<T> std::ops::Deref for Global<T>{
-    type Target = Mutex<T>;
+impl<T> std::ops::Deref for RawGlobal<T> {
+    type Target = T;
 
-    //Third, you can use global variables like std::sync::Mutex.
-    fn deref(&self) -> &Mutex<T> {
+    fn deref(&self) -> &Self::Target {
         match self {
-            Global::No => panic!("You have to do .set method before use this."),
-            Global::Yes(ref a) => a,
+            RawGlobal::No(_) => panic!("Set value to this variable."),
+            RawGlobal::Yes(a) => a,
         }
     }
 }
